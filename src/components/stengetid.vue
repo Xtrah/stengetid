e281a1598cb1d74e3a3ced12653425da44161d00
Xtrah <xtrah@live.com>
2021-05-17 23:18:27 +0200

    refactor to API with Azure Functions

--- src/components/stengetid.vue
+++ src/components/stengetid.vue
@@ -2,7 +2,6 @@
   <div class="hello">
     <h1>{{ closingTime }}</h1>
     <p>{{ timeLeft }}</p>
-    <p></p>
   </div>
 </template>
 
@@ -11,63 +10,14 @@ export default {
   name: "stengetid",
   data() {
     return {
-      timeLeft: null,
-      closingTime: null
+      closingTime: null,
+      timeLeft: null
     };
   },
   async created() {
-    const VUE_APP_apikey = process.env.VUE_APP_apikey
-    const headers = {"Ocp-Apim-Subscription-Key": VUE_APP_apikey};
-    const response = await fetch("https://apis.vinmonopolet.no/stores/v0/details?storeID=160", { headers })
-    if (response.ok) {
-      var data = await response.json();
-      data = data[0]
-    } else {
-      alert("HTTP-Error: " + response.status);
-    }
-
-    // Find current time and day
-    const now = new Date()
-    var todayDate = now.toLocaleDateString('en-CA'); // YYYY-MM-DD
-    var currentDay = now.getDay()-1
-    if (currentDay == -1) { 
-      currentDay = 6 // API Sunday
-    }
-
-    // Use Day.js to calculate time differences
-    var dayjs = require('dayjs')
-    require('dayjs/locale/nb')
-    // import dayjs from 'dayjs' // ES 2015
-    dayjs().format()
-    dayjs.locale('nb') // use locale globally
-    var customParseFormat = require('dayjs/plugin/customParseFormat')
-    dayjs.extend(customParseFormat)
-    var relativeTime = require('dayjs/plugin/relativeTime')
-    // import relativeTime from 'dayjs/plugin/relativeTime' // ES 2015
-    dayjs.extend(relativeTime)
-    
-    // Return relevant info
-    if (data.openingHours.regularHours[currentDay].closed == true) {
-      this.closingTime = "Stengt i dag"
-      // TODO: Next opening hour
-    } else {
-      this.closingTime = data.openingHours.regularHours[currentDay].closingTime
-      let closingTime = dayjs(this.closingTime, "HH:mm")
-      this.timeLeft = dayjs().to(closingTime)
-    }
-
-    for (let i = 0; i < data.openingHours.exceptionHours.length; i++) {
-      if (data.openingHours.exceptionHours[i].date == todayDate) {
-        if (data.openingHours.exceptionHours[i].message != "") {
-        this.closingTime = data.openingHours.exceptionHours[i].message
-        }
-        else {
-          this.closingTime = data.openingHours.exceptionHours[i].closingTime
-          let closingTime = dayjs(this.closingTime, "HH:mm")
-          this.timeLeft = dayjs().to(closingTime)
-        }
-      }
-    }
+    const { text } = await (await fetch("/api/closingTime")).json();
+    this.closingTime = text[0];
+    this.timeLeft = text[1]
 
   }
 };
