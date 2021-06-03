b39e1e891db2b6a4ca7238357f7f7e554551a8ba
Panda <31665764+Burning-Panda@users.noreply.github.com>
2021-06-03 12:21:21 +0200

    add tailwindcss
    Added tailwind css to the project

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,18 @@
 <template>
-  <!--<img alt="polrose" style="width:200px" src="./assets/logo.jpg">-->
-  <h1>Stenge<span class="thin">tid</span></h1>
-  <stengetid/>
-  
-  <!-- Props can be added or changed easily, remmove the v-for if not needed-->
-  <div class="card_container">
-    <card 
-      v-for="n in 3" v-bind:key="n"
+  <div class="container max-w-screen-lg mx-auto md:mt-20">
+    <h1 class="text-center font-serif font-semibold text-8xl">Stenge<span class="font-light">tid</span></h1>
+    <stengetid/>
 
-      img_src="testMap.jpg"
-      cardTitle="Rekker jeg det?"
-      cardContent="Vil du vite om du rekker å dra til Vinmonopolet før det stenger? Da kan du klikke her!"
-      cardButton="CAN I MAKE IT?"></card>
+    <!-- Props can be added or changed easily, remmove the v-for if not needed-->
+    <div class="card_container flex justify-evenly gap-10 mt-20">
+      <card
+          v-for="n in 3" v-bind:key="n"
+
+          img_src="testMap.jpg"
+          cardTitle="Rekker jeg det?"
+          cardContent="Vil du vite om du rekker å dra til vinmonopolet før det stenger?"
+          cardButton="Button"></card>
+    </div>
   </div>
 </template>
 
@@ -36,42 +37,11 @@ html {
   background-color:#1C1C1E;
   background:#1C1C1E;
   color:#E8EAED;
-  font-family: 'Source Serif Pro', serif;
-  text-align: center;
-  font-style: normal;
-  /* Font size set to 10px for easier use of em for sizing
-     Its also useful for scaling the UI
-    (ex. 2em = 10px*2) */
-  font-size:10px;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
 }
 
 #app {
-  /*font-family: Avenir, Helvetica, Arial, sans-serif;*/
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  /*color: #2c3e50;*/
-  margin-top: 60px;
-}
-
-h1 {
-  font-weight:600;
-  font-size:6.4em;
-}
-h1 span.thin{font-weight:300}
-
-
-.card_container {
-    /* Container Design */
-    max-width:920px;
-    padding:10px;
-    margin: 10em auto 0 auto;
-
-    /* inner area */
-    text-align: left;
-
-    display: grid;
-    grid-template-columns: 1fr 1fr 1fr;
-    grid-gap:2em;
 }
 
 </style>
---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+module.exports = {
+    purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      colors: {
+        black: {DEFAULT: '#1c1c1e'},
+        gray: {DEFAULT: '#2c2c2e'},
+        white: {DEFAULT: '#e8eaed'},
+        green: {DEFAULT: '#6aeb9d'},
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+        serif: ['Source Serif Pro', 'serif']
+      },
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  
